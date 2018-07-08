a09fb9d42f5df742d9dca93f932fe321c05377e7
Maamar Dliouah <maamardli@windowslive.com>
2018-07-08 21:11:46 +0100

    Testing the Iris Model

--- MachineLearningTest01/Program.cs
+++ MachineLearningTest01/Program.cs
@@ -3,13 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Models;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
 
 namespace MachineLearningTest01
 {
 	class Program
 	{
+		public class IrisData
+		{
+			[Column("0")]
+			public float SepalLength;
+
+			[Column("1")]
+			public float SepalWidth;
+
+			[Column("2")]
+			public float PetalLength;
+
+			[Column("3")]
+			public float PetalWidth;
+
+			[Column("4")]
+			[ColumnName("Label")]
+			public string Label;
+
+		}
+
+		public class IrisPrediction
+		{
+			[ColumnName("PredictedLabel")]
+			public string PredictedLabels;
+		}
+
 		static void Main(string[] args)
 		{
+			var pipeLine = new LearningPipeline();
+
+			var dataPath = "IrisData.txt";
+
+			pipeLine.Add(new TextLoader(dataPath).CreateFrom<IrisData>(separator: ','));
+			pipeLine.Add(new Dictionarizer("Label"));
+			pipeLine.Add(new ColumnConcatenator("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"));
+			pipeLine.Add(new StochasticDualCoordinateAscentClassifier());
+			pipeLine.Add(new PredictedLabelColumnOriginalValueConverter() { PredictedLabelColumn = "PredictedLabel" });
+
+			var model = pipeLine.Train<IrisData, IrisPrediction>();
+			var prediction = model.Predict(new IrisData()
+			{
+				SepalLength = 3.3f,
+				SepalWidth = 1.6f,
+				PetalLength = 0.2f,
+				PetalWidth = 5.1f,
+			});
+
+			Console.WriteLine($"Predicted flower type is: {prediction.PredictedLabels}");
+			Console.ReadLine();
 		}
 	}
 }
